14b05117b57649753bbb3f5d31d8b82fc57c692a
Scott Morse <scottmorsedev@gmail.com>
2018-12-05 17:46:16 -0600

    Production mode

--- devServer.js
+++ devServer.js
@@ -6,12 +6,12 @@ var config = require('./webpack.config.prod');
 var app = express();
 var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+// app.use(require('webpack-dev-middleware')(compiler, {
+//   noInfo: true,
+//   publicPath: config.output.publicPath
+// }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+// app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
