485e09a85240822f66098a24f533900a6ebccb5d
Scott Morse <scottmorsedev@gmail.com>
2018-12-05 18:24:21 -0600

    Hopefully fixing production mode bug in express server

--- devServer.js
+++ devServer.js
@@ -6,12 +6,12 @@ var config = require('./webpack.config.dev');
 var app = express();
 var compiler = webpack(config);
 
-// app.use(require('webpack-dev-middleware')(compiler, {
-//   noInfo: true,
-//   publicPath: config.output.publicPath
-// }));
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-// app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-hot-middleware')(compiler));
 
 app.use('/', express.static(path.join(__dirname, 'dist')))
 
